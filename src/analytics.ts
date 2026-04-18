435ca9bd0c382c4d55ba1915b1082bd902f6b242
charlesmagutu11 <info@condabu.com>
2026-04-18 22:20:46 +0300

    fixed gtag not loading

--- src/analytics.ts
+++ src/analytics.ts
@@ -2,13 +2,20 @@ import ReactGA from "react-ga4";
 
 const GA_ID = "G-742TNTTLQ2";
 
+let initialized = false;
+
 export const initGA = () => {
-  ReactGA.initialize(GA_ID);
+  if (!initialized) {
+    ReactGA.initialize(GA_ID);
+    initialized = true;
+  }
 };
 
-export const trackPageView = (path: string) => {
+export const trackPageView = (page: string) => {
+  if (!initialized) initGA();
+
   ReactGA.send({
     hitType: "pageview",
-    page: path,
+    page,
   });
 };
